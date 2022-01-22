a45c6f6061175a52cf416c0cc24addab4cd45e04
Benedict Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2022-01-22 14:53:01 +0100

    Add files via upload

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import {TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem} from "@material-ui/core";
+import { ExpenseTrackerContext } from '../../../context/context';
+import {v4 as uuidv4} from "uuid";
+import { incomeCategories, expenseCategories } from '../../../constants/categories';
+import formatDate from '../../../utils/formatDate';
 import useStyles from "./styles";
+
+const initialState = {
+    amount: "",
+    category: "",
+    type: "income",
+    date: formatDate(new Date()),
+}
 
 const Form = () => {
     const classes = useStyles();
+    const [formData, setFormData] = useState(initialState);
+    const {addTransaction} = useContext(ExpenseTrackerContext);
+
+    const createTransaction = () => {
+        const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4()}
+        addTransaction(transaction);
+        setFormData(initialState);
+    }
+
+    const selectedCatergories = formData.type === "Income" ? incomeCategories: expenseCategories;
+
     return (
         <Grid container spacing={2}>
             <Grid item xs={12}>
@@ -14,7 +36,7 @@ const Form = () => {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Type</InputLabel>
-                    <Select>
+                    <Select value={formData.type} onChange={(e) => setFormData({ ...formData, type: e.target.value})}>
                         <MenuItem value="Income">Income</MenuItem>
                         <MenuItem value="Expense">Expense</MenuItem>
                     </Select>
@@ -23,19 +45,18 @@ const Form = () => {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Catergory</InputLabel>
-                    <Select>
-                        <MenuItem value="business">Business</MenuItem>
-                        <MenuItem value="salary">Salary</MenuItem>
+                    <Select value={formData.category} onChange={(e) => setFormData({ ...formData, category: e.target.value})}>
+                        {selectedCatergories.map((c) => <MenuItem key={c.type} value={c.type}>{c.type}</MenuItem>)}
                     </Select>
                 </FormControl>
             </Grid>
             <Grid item xs={6}>
-                <TextField type="number" label="Amount" fullWidth/>
+                <TextField type="number" label="Amount" fullWidth value={formData.amount} onChangeCapture={(e) => setFormData({ ...formData, amount: e.target.value})}/>
             </Grid>
             <Grid item xs={6}>
-                <TextField type="date" label="Date" fullWidth/>
+                <TextField type="date" label="Date" fullWidth value={formData.date} onChangeCapture={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })}/>
             </Grid>
-            <Button className={classes.button} variant="outlined" color="primary" fullWidth>Create</Button>
+            <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}>Create</Button>
         </Grid>
     )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Card, CardHeader, CardContent, Typography, Grid, Divider} from '@material-ui/core'
 import useStyles from "./styles";
+import Form from "./Form/Form";
+import List from "./List/List";
 
 const Main = () => {
     const classes = useStyles()
@@ -10,15 +12,16 @@ const Main = () => {
             <CardContent>
                 <Typography align="center" variant="h5">Total Balance $100</Typography>
                 <Typography variant="subtitle1" style={{ lineHeight: "1.5em", marginTop: "20px"}}>
-                    Try saying: Add income fro $100 in catergory slary for Monday
+                    {/* <InfoCard/> */}
+                    Try saying: Add income fro $100 in catergory salary for Monday
                 </Typography>
                 <Divider/>
-                {/* <Form/> */}
+                <Form/>
             </CardContent>
             <CardContent className={classes.cardContent}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        {/* <List/> */}
+                        <List />
                     </Grid>
                 </Grid>
             </CardContent>
