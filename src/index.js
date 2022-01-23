325c372f9cb46071a47b7316665bdfe6dbbacadb
Benedict Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2022-01-23 21:40:50 +0100

    Add Speechly library

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from "react-dom";
+import {SpeechProvider} from "@speechly/react-client";
 import {Provider} from "./context/context";
 
 import App from "./App";
 import "./index.css"
 
 ReactDOM.render(
-    <Provider>
-        <App/>
-    </Provider>,
+    <SpeechProvider appId="f70d9350-f2d4-4e13-ad65-3b36db274fe2" language="en-US">
+        <Provider>
+            <App/>
+        </Provider>
+    </SpeechProvider>,
     document.getElementById("root")
 )
